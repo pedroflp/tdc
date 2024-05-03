133a2faf6eefe62dc01ebef79bb63595b8d51a1c
Pedro Lima <pedro.lima@wefit.com.br>
2024-05-02 22:51:15 -0300

    chore: trocando web head title and description, and adding BETA badge in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,12 @@ import Link from 'next/link';
 import ProfileDropdown from './components/ProfileDropdown';
 import { SignDialog } from './components/SignDialog';
 import { UserDTO } from '@/app/api/user/types';
+import { Badge } from '../ui/badge';
 
 export default async function Header({ user }: { user: UserDTO }) {
   return (
     <div className='w-screen border-b-[1px] border-slate-300 p-4 px-16 flex justify-between items-center'>
-      <Link href={'/'} className='text-2xl font-black text-slate-800'>TDCU</Link>
+      <Link href={'/'} className='text-2xl font-black text-slate-800 flex gap-2 items-center'>TDCU <Badge>BETA</Badge></Link>
       {!!user ? <ProfileDropdown user={user} /> : <SignDialog />}
     </div>
   )
