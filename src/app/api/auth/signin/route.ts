509d9bd252d82493b9ba0de8a0b4e755f9e5fcea
Pedro Lima <pedro.lima@wefit.com.br>
2024-07-28 19:33:17 -0300

    refactor: discord auth user can migrate user data when change discord username

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -2,21 +2,35 @@ import { cookiesKeys } from "@/constants/cookies";
 import { collections } from "@/services/constants";
 import discordAuth from "@/services/discord";
 import { firestore } from "@/services/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc, where,  } from "firebase/firestore";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { signUp } from "../signup/requests";
+import { UserDTO } from "../../user/types";
 
 export async function POST(request: NextRequest) {
   const { accessToken, expires, refreshToken } = await request.json();
 
   const { id, avatar, username: discordUsername, email } = await discordAuth.getUser(accessToken);
-  const user = await getDoc(doc(firestore, collections.USERS, discordUsername));
 
-  if (!user.exists()) {
+  const queryIfUserIdAlreadyExistsInOtherUsername = query(collection(firestore, collections.USERS), where("id", "==", id));
+  const queryUser = (await getDocs(queryIfUserIdAlreadyExistsInOtherUsername)).docs[0];
+
+  if (!queryUser.exists()) {
     await signUp({ avatar, email, id, username: discordUsername });
   }
 
+  const queryUserData = queryUser.data() as UserDTO;
+
+  if (queryUserData.username !== discordUsername) {
+    await setDoc(doc(firestore, collections.USERS, discordUsername), {
+      ...queryUserData,
+      username: discordUsername
+    })
+
+    await deleteDoc(doc(firestore, collections.USERS, queryUserData.username));
+  }
+  
   cookies().set(cookiesKeys.TOKEN, accessToken, { expires: new Date().setSeconds(expires) });
   cookies().set(cookiesKeys.REFRESH_TOKEN, refreshToken);
   
